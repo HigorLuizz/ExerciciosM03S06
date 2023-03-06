373a720b486c04b8a082b64349c99b3191c8c455
douglas-cavalcante <henriquedouglas2803@gmail.com>
2023-03-06 19:27:14 -0300

    adiciona projeto inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <div>
+      <div className="main-container">
+        <div data-testid="products-list" className="products-list">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
