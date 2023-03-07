a9570cb15aef7daf6dd2d208cdcf1e57395168d1
douglas-cavalcante <henriquedouglas2803@gmail.com>
2023-03-06 22:02:57 -0300

    feat: adiciona tela de detalhes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Cart from '../pages/Cart';
+import Details from '../pages/Details';
 import Home from '../pages/Home';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="cart" element={<Cart />} />
+      <Route path="carrinho" element={<Cart />} />
+      <Route path="detalhes" element={<Details />} />
     </Routes>
   );
 }
