b317f97a006861f2a6ac6fab7ef2910270f7b2c6
douglas-cavalcante <henriquedouglas2803@gmail.com>
2023-03-08 20:32:40 -0300

    adiciona teste de funcao

--- src/utils/generateDiscount.js
+++ src/utils/generateDiscount.js
@@ -1,10 +1,11 @@
 export const generateDiscount = (valor, percentual) => {
-  const desconto = valor * percentual / 100;
+  const desconto = Number(valor) * Number(percentual) / 100;
   const valorComDesconto = valor - desconto;
   const porcentagemFormatada = `${percentual}%`;
 
   return {
     porcentagem: porcentagemFormatada,
-    valorComDesconto: valorComDesconto
+    valorComDesconto: Number(valorComDesconto.toFixed(2))
   };
+
 }
