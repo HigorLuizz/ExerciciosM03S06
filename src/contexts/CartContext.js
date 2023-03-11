b22235e479a4bbd0b22f507847271c42319aef35
douglas-cavalcante <henriquedouglas2803@gmail.com>
2023-03-10 21:38:51 -0300

    adiciona carrinho

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,26 +1,24 @@
 // inicializar o contexto
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 import { v4 as uuidv4 } from 'uuid';
-import { generateDiscount } from "../utils/generateDiscount";
 import { priceFormat } from "../utils/priceFormat";
 
 export const CartContext = createContext()
 
 // Montar o provedor
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
 
   const [cart, setCart] = useState([])
 
-
   const addProduct = (product, observations, amount, priceWithDiscount) => {
-
-    console.log("o desconto é ", product.discount)
 
     const data = {
       itemId: uuidv4(),
       product,
-      priceWithDiscount: priceFormat(priceWithDiscount),
+      priceWithDiscountFormatted: priceFormat(priceWithDiscount),
+      priceWithDiscount,
       observations,
       amount,
       subTotal: priceWithDiscount * amount,
@@ -30,10 +28,34 @@ export const CartProvider = ({children}) => {
     setCart([...cart, data])
   }
 
+  const changeAmount = (itemId, amount) => {
+    setCart(cart.map(item => {
+      if (item.itemId === itemId) {
+        item.amount = amount
+        item.subTotal = item.priceWithDiscount * amount
+        item.subTotalFormatted = priceFormat(item.priceWithDiscount * amount)
+      }
+      return item
+    }))
+  }
+
+  const removeItem = (itemId) => {
+    setCart(cart.filter(item => {
+      if (item.itemId !== itemId) {
+        return true
+      } else {
+        return false
+      }
+    }))
+    toast.success('Deletado com sucesso')
+  }
+
   return (
     <CartContext.Provider value={{
       cart: cart,
-      addProduct: addProduct
+      addProduct: addProduct,
+      removeItem: removeItem,
+      changeAmount: changeAmount
     }}>
       {children}
     </CartContext.Provider>
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,29 @@
 import { useContext } from 'react'
 import { FaMinusCircle, FaPlusCircle, FaTrashAlt } from 'react-icons/fa'
+import Swal from 'sweetalert2'
+
 import { CartContext } from '../../contexts/CartContext'
+import { priceFormat } from '../../utils/priceFormat'
 
 const Cart = () => {
+  const { cart, removeItem, changeAmount } = useContext(CartContext)
 
-  const {cart} = useContext(CartContext)
+  const total = priceFormat(cart.reduce((acc, currentItem) => {
+    return acc + currentItem.subTotal
+  }, 0))
 
+  const handleDeleteItem = (itemId) => {
+    Swal.fire({
+      title: 'Deseja realmente remover esse item ?',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, desejo !',
+      cancelButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        removeItem(itemId)
+      }
+    })
+  }
 
   return (
     <>
@@ -28,30 +46,32 @@ const Cart = () => {
                   <img className='table-image' src={item.product.image} alt={item.product.name} />
                 </td>
                 <td>{item.product.name}</td>
-                <td>{item.priceWithDiscount}</td>
+                <td>{item.priceWithDiscountFormatted}</td>
                 <td>
                   <FaMinusCircle
                     size={18}
                     color="#9721BD"
                     className='margin-button'
+                    onClick={() => changeAmount(item.itemId, item.amount - 1)}
                   />
                   {item.amount}
                   <FaPlusCircle
                     size={18}
                     color="#9721BD"
                     className='margin-button'
+                    onClick={() => changeAmount(item.itemId, item.amount + 1)}
                   />
                 </td>
                 <td>{item.subTotalFormatted}</td>
                 <td>
-                  <FaTrashAlt size={22} color='#9721BD'/>
+                  <FaTrashAlt size={22} color='#9721BD' onClick={() => handleDeleteItem(item.itemId)}/>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
         <div className='footer-cart'>
-          <h1>Total: 0</h1>
+          <h1>Total: {total}</h1>
           <button>Continuar compra</button>
         </div>
       </div>
